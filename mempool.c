20bf56fecafd159b7abe97ed6dac20f95fe21822
Gaurav Kumar Garg <ggarg@redhat.com>
2014-09-26 11:52:03 +0530

    This is mempool porgramme basic routine.

---
+++ mempool.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct POOL
+{
+        char *next;
+        char *end;
+}pool;
+
+pool *pool_create (size_t size)
+{
+        pool *p = (pool*)malloc (size + sizeof (pool));
+        p->next = (char*)&p[1];
+        p->end  = p->next + size;
+
+        return p;
+}
+
+void pool_destroy (pool *p) {
+        free (p);
+        p = NULL;
+}
+
+size_t pool_available (pool *p) {
+        return p->end - p->next;
+}
+
+void *pool_alloc (pool *p, size_t size) {
+        if (pool_available(p) < size) {
+                printf ("oops!!! No memory left in mempool\n");
+                return NULL;
+        }
+        void *mem = (void *)p->next;
+        p->next +=size;
+        printf ("memeory successfully allocated from pool\n");
+        return mem;
+}
+
+int main ()
+{
+        pool *my_pool = pool_create (100);
+        pool *use1,*use2;
+
+        printf ("pool_available is %d\n\n",pool_available(my_pool));
+
+        use1 = pool_alloc (my_pool, 99);
+        printf ("pool_available is %d\n\n",pool_available(my_pool));
+
+        use1 = pool_alloc (my_pool, 50);
+        printf ("pool_available is %d\n\n",pool_available(my_pool));
+
+        return 0;
+}
